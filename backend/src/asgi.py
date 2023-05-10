80f41ca73ac332663c9a7294d026151fa0c6c0cd
Yevhenii Korolikhin <yevhenii@devforth.io>
2023-05-10 21:53:17 +0300

    add alembic

--- backend/src/asgi.py
+++ backend/src/asgi.py
@@ -1,9 +1,4 @@
 from .main import init
-import asyncio
-from .config import init_models
-
 
 app = init()
 
-if __name__ == '__main__':
-    asyncio.run(init_models())
--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -7,3 +7,10 @@ class Person(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     age = Column(Integer)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    age = Column(Integer)
