7c7c13fe22c1d9be400c789da9549425b4c18a5b
Yevhenii Korolikhin <yevhenii@devforth.io>
2023-05-10 21:57:54 +0300

    clean project

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -1,16 +1,2 @@
 from src.config import Base
 from sqlalchemy import  Column, Integer, String
-
-class Person(Base):
-    __tablename__ = "people"
- 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    age = Column(Integer)
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    age = Column(Integer)
--- backend/src/views/index.py
+++ backend/src/views/index.py
@@ -1,36 +1,10 @@
-from fastapi import APIRouter, FastAPI, Depends, Body
-from src.config import get_db
-from sqlalchemy.ext.asyncio import AsyncSession
-from src.models.user import Person
-from sqlalchemy import select
+from fastapi import APIRouter, FastAPI
+
 router = APIRouter()
 
 @router.get("/")
 async def index():
     return {"message": "Hello, world!"}
-
-@router.get("/api/users")
-async def get_people(db: AsyncSession = Depends(get_db)):
-    result = await db.execute(select(Person).order_by(Person.id))
-    
-    response = [{
-        "id": p.id,
-        "name": p.name,
-        "age": p.age
-    } for p in result.scalars().all()]
-
-    return response
-
-@router.post("/api/users")
-async def create_person(data  = Body(), db: AsyncSession = Depends(get_db)):
-    person = Person(name=data["name"], age=data["age"])
-    db.add(person)
-
-    await db.commit()
-    await db.refresh(person)
-
-    return person
-
 
 
 def init_app(app: FastAPI):
